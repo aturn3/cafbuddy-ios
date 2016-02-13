5b7eddab00177a38c812a7b402c2cdd37bb4f494
Jacob Forster <jforster959@gmail.com>
2016-02-12 19:10:33 -0600

    Have the basic layout of the settings / profile screen done. It works for all screen sizes right now. Now just need to implement the buttons actually and then add the logo for cafbuddy once we have one at the top of the screen.

--- CafBuddy/CafBuddy/ProfileViewController.swift
+++ CafBuddy/CafBuddy/ProfileViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class ProfileViewController: MainScreenViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var scrollViewMain = UIScrollView()
     var tableViewMainOptions = UITableView()
-//    let
+    let settingsOptions = [
+        ["Your Compliments"],
+        ["How CafBuddy Works", "Terms of Service", "Privacy Policy", "Credits"],
+        ["Log Out"]
+    ]
+    var rowHeight = CGFloat(50)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Set the title of the navigation bar
+        navigationItem.title = "Meal History"
         
         initInterface()
     }
@@ -25,17 +35,41 @@ class ProfileViewController: MainScreenViewController, UITableViewDelegate, UITa
     }
     
     func initInterface() {
+        // based on whatever the row height is, it will leave the margin at what is if the user already has to scroll
+        // otherwise it will increase the margin above the table (where the image goes) if there should be extra space
+        // there so that the last button is at the bottom of the screen
+        var marginTopTable = CGFloat(150)
+        var totalSpaces = 0
+        for optionsList in settingsOptions {
+            totalSpaces += optionsList.count + 1
+        }
+        totalSpaces -= 1 // account for last space not being there since last section
         
+        let spaceAvailable = screenSize.height - STATUS_BAR_HEIGHT - TAB_BAR_HEIGHT - NAV_BAR_HEIGHT
+        let currentSpaceTaken = (CGFloat(totalSpaces) * rowHeight) + marginTopTable
+        if (currentSpaceTaken < spaceAvailable){
+            marginTopTable += (spaceAvailable - currentSpaceTaken)
+        }
+        let newSpaceTaken = (CGFloat(totalSpaces) * rowHeight) + marginTopTable
+
+        scrollViewMain.frame = CGRectMake(0, 0, screenSize.width, spaceAvailable)
+        scrollViewMain.contentSize = CGSizeMake(screenSize.width, newSpaceTaken)
+        scrollViewMain.scrollEnabled = true
+        scrollViewMain.delegate = self
+        scrollViewMain.backgroundColor = COLOR_NEUTRAL_BACKGROUND
         
         tableViewMainOptions.delegate = self
         tableViewMainOptions.dataSource = self
-        tableViewMainOptions.rowHeight = 50
-        tableViewMainOptions.frame = CGRectMake(0, 0, screenSize.width, screenSize.height)
+        tableViewMainOptions.scrollEnabled = false
+        tableViewMainOptions.rowHeight = rowHeight
+        tableViewMainOptions.frame = CGRectMake(0, marginTopTable, screenSize.width, screenSize.height)
         tableViewMainOptions.backgroundColor = COLOR_NEUTRAL_BACKGROUND
         tableViewMainOptions.registerClass(UITableViewCell.self, forCellReuseIdentifier: "settingsCell")
+        tableViewMainOptions.registerClass(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "footerView")
         
         
-        self.view.addSubview(tableViewMainOptions)
+        self.view.addSubview(scrollViewMain)
+        scrollViewMain.addSubview(tableViewMainOptions)
     }
     
     
@@ -44,22 +78,32 @@ class ProfileViewController: MainScreenViewController, UITableViewDelegate, UITa
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let settingsCell = tableView.dequeueReusableCellWithIdentifier("settingsCell", forIndexPath: indexPath)
         
-        if (indexPath.row == 0) {
-            settingsCell.textLabel?.text = "FAQ"
-        }
-        else {
-            settingsCell.textLabel?.text = "Terms of Service"
-        }
+        settingsCell.textLabel?.text = settingsOptions[indexPath.section][indexPath.row]
+        settingsCell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
         
         return settingsCell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return settingsOptions[section].count
+    }
+
+    
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footer = tableView.dequeueReusableHeaderFooterViewWithIdentifier("footerView")
+        return footer
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        // all footers should be 50 in size except last shouldn't have a footer since at bottom
+        if (section != settingsOptions.count - 1) {
+            return rowHeight
+        }
+        return 0
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return settingsOptions.count
     }
     
     
